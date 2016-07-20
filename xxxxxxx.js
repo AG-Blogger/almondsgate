fa127f35b8b303141f27b373b291a9a56d0f7c9f
AG-Blogger <phone76w@yahoo.com>
2016-07-20 06:01:28 +0300

    Delete xxxxxxx.js

--- xxxxxxx.js
+++
@@ -1,7 +0,0 @@
-<img class="inlarge" src="https://placehold.it/350x150?text=Normal+Image">
-<img class="inlarge" data-large="http://placehold.it/1600x800?text=This+Is+Large!" src="https://placehold.it/350x150?text=Large+Image">
-<img class="inlarge" data-mobile="http://placehold.it/700x300?text=Mobile+Eyes+Only" src="https://placehold.it/350x150?text=Normal+and+Mobile">
-<img class="inlarge" data-large="http://placehold.it/1600x800?text=This+Is+Large!" data-mobile="http://placehold.it/700x300?text=Mobile+Eyes+Only" src="https://placehold.it/350x150?text=Large+and+Mobile">
-<img class="inlarge" data-iframe="http://justinarmstrongcreative.com" src="https://placehold.it/350x150?text=Iframe">
-<img class="inlarge" data-iframe="https://www.youtube.com/embed/Hv_DRJZZ2qI" src="https://placehold.it/350x150?text=Large+Image">
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://placehold.it/350x150?text=Vimeo"> 
