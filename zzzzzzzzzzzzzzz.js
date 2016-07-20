3335e3852393553f8c353abfd7696656bad7b625
AG-Blogger <phone76w@yahoo.com>
2016-07-20 06:01:45 +0300

    Create zzzzzzzzzzzzzzz.js

---
+++ zzzzzzzzzzzzzzz.js
@@ -0,0 +1,7 @@
+document.write("<img class=\"inlarge\" src=\"https:\/\/placehold.it\/350x150?text=Normal+Image\">");
+document.write("<img class=\"inlarge\" data-large=\"http:\/\/placehold.it\/1600x800?text=This+Is+Large!\" src=\"https:\/\/placehold.it\/350x150?text=Large+Image\">");
+document.write("<img class=\"inlarge\" data-mobile=\"http:\/\/placehold.it\/700x300?text=Mobile+Eyes+Only\" src=\"https:\/\/placehold.it\/350x150?text=Normal+and+Mobile\">");
+document.write("<img class=\"inlarge\" data-large=\"http:\/\/placehold.it\/1600x800?text=This+Is+Large!\" data-mobile=\"http:\/\/placehold.it\/700x300?text=Mobile+Eyes+Only\" src=\"https:\/\/placehold.it\/350x150?text=Large+and+Mobile\">");
+document.write("<img class=\"inlarge\" data-iframe=\"http:\/\/justinarmstrongcreative.com\" src=\"https:\/\/placehold.it\/350x150?text=Iframe\">");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/www.youtube.com\/embed\/Hv_DRJZZ2qI\" src=\"https:\/\/placehold.it\/350x150?text=Large+Image\">");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/placehold.it\/350x150?text=Vimeo\">");
