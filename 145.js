6ad5028203cbeb725e5b5ab0a774d9f67137efb5
GM-Blogger <phone76w@yahoo.com>
2016-07-28 12:50:59 +0300

    Update 145.js

--- 145.js
+++ 145.js
@@ -1,29 +1,29 @@
-<h3>
-<div style="text-align: center; text-shadow: 4px 4px 2px rgb(244, 61, 61);">
-ДНЕВНИК ЭКСТРАСЕНСА С ФАТИМОЙ ХАДУЕВОЙ
-<br />
-1 СЕЗОН
-</div>
-</h3>
-<br />
-<br />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/9lOz2I" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/UsVmIK" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/yh0qEh" style="margin: 0px 0px 15px 15px;" width="220" /><br />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/vce4GQ" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/BwrRyl" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/5s1qzy" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/9yN72g" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/D9Ihii" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/saKfd3" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/9JOm12" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/dCLRQ3" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/7CJY4I" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/tVEdo5" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/sGN6Yg" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/QvVuvL" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/4plKFn" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/r2q6ue" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/i7phkr" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/e3Hglo" style="margin: 0px 0px 15px 15px;" width="220" />
-<img class="inlarge" data-iframe="https://player.vimeo.com/video/61483000" src="https://goo.gl/Bws9r7" style="margin: 0px 0px 15px 15px;" width="220" />
+document.write("<h3>");
+document.write("<div style=\"text-align: center; text-shadow: 4px 4px 2px rgb(244, 61, 61);\">");
+document.write("ДНЕВНИК ЭКСТРАСЕНСА С ФАТИМОЙ ХАДУЕВОЙ");
+document.write("<br \/>");
+document.write("1 СЕЗОН");
+document.write("<\/div>");
+document.write("<\/h3>");
+document.write("<br \/>");
+document.write("<br \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/9lOz2I\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/UsVmIK\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/yh0qEh\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/><br \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/vce4GQ\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/BwrRyl\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/5s1qzy\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/9yN72g\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/D9Ihii\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/saKfd3\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/9JOm12\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/dCLRQ3\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/7CJY4I\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/tVEdo5\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/sGN6Yg\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/QvVuvL\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/4plKFn\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/r2q6ue\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/i7phkr\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/e3Hglo\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
+document.write("<img class=\"inlarge\" data-iframe=\"https:\/\/player.vimeo.com\/video\/61483000\" src=\"https:\/\/goo.gl\/Bws9r7\" style=\"margin: 0px 0px 15px 15px;\" width=\"220\" \/>");
